641ba5fb3c2c76339b6a6db2bd7c71ecb2059164
Aarya Nikam <169072273+aaryanikam@users.noreply.github.com>
2025-06-29 20:41:48 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
---
+++ views.py
@@ -0,0 +1,8 @@
+from django import views
+from django.shortcuts import render
+
+import core
+import rbac_project
+
+# Create your views here.
+rbac_project/core/views.py
